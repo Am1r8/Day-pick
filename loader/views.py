05d15341a82afe89f45d8d2a1858e14b774d8089
AlPHA__830 <68626539+CoachAlpha@users.noreply.github.com>
2022-04-10 00:30:49 -0400

    adding the app to repo

---
+++ loader/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render
+import requests
+from urllib.request import urlretrieve
+
+apiKey = 'g3oEjV4urmjOu47SfmDgIvTMvnDq2aD8awgd0Sbu'
+
+
+def show(request):
+    URL_APOD = "https://api.nasa.gov/planetary/apod"
+    date = '2020-02-22'
+    params = {
+        'date': date,
+        'api_key':apiKey,
+        'hd':'True'
+    }
+    response = requests.get(URL_APOD,params=params).json()
+    print(response)
+    data= {'url': response.get('hdurl'), 'explanation': response.get('explanation'), 'tittle': response.get('title')}
+    context= {
+        'data': data
+    }
+    return render(request, "home.html", context)
+
+
