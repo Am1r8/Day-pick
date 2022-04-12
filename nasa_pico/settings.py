1ecffccb32f1c9c2139aedbb7eb35c239ed77015
AlPHA__830 <68626539+CoachAlpha@users.noreply.github.com>
2022-04-12 12:33:46 -0400

    new

--- nasa_pico/settings.py
+++ nasa_pico/settings.py
@@ -48,6 +48,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 django_heroku.settings(locals())
